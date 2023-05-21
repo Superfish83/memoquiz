c1a82a0c68e8f8e15f48bed67e760b51c4e693cc
연준 김 <kyjun050803@gmail.com>
2023-05-22 00:50:38 +0900

    tailwind 설정

---
+++ pages/index.jsx
@@ -0,0 +1,11 @@
+
+
+export default function Index(){
+    return (
+    <div className="mx-auto w-[500px] h-[800px] m-4 p-4 rounded-xl shadow-2xl">
+        <div className="font-extrabold text-xl text-center">
+            Hello 퀴즈사이트!
+        </div>
+    </div>
+    );
+}
