d33ecc4db15c5afd71818aa8c986fa334733fd5e
연준 김 <kyjun050803@gmail.com>
2023-06-07 15:24:13 +0900

    add quiz kr 4

--- pages/Korean/quiz/[quizid].jsx
+++ pages/Korean/quiz/[quizid].jsx
@@ -3,6 +3,7 @@ import { useRouter } from "next/router";
 import { useCallback, useEffect, useState } from 'react';
 import { shuffle } from 'lodash'
 import { News_Cycle } from "next/font/google";
+import { Link } from "react-router-dom";
 
 export default function Quiz(){
     const router = useRouter();
@@ -33,7 +34,7 @@ export default function Quiz(){
         .then(response => {
           return response.json();
         }).then(data => {
-          let shfData = shuffle(data);
+          let shfData = data;//shuffle(data);
           let selData = []
           for(let i = 0; i < data.length; i++){
             let item = shfData[i];
@@ -105,18 +106,39 @@ export default function Quiz(){
     function Question({id, data, sel}){
         const num = id;
         const query = data.question;
+        const text = data.text;
+        const refs = data.refs;
         const options = data.options;
         return (
             <div className="p-2">
                 <div className="text-xl font-bold">문제 {num+1}</div>
                 <div className="pt-2 pb-4">{query}</div>
+                {
+                    text ? 
+                    (
+                        <div className="border-2 border-slate-600 rounded-xl mx-1 my-4 p-2">
+                            <div className="font-bold text-center">[보기]</div>
+                            {text}
+                            <div className="p-2 text-slate-400">
+                                출처
+                                {refs?.map((data, key) => (
+                                    <a 
+                                        className="text-blue-400"
+                                        target='_blank'
+                                        href={data} 
+                                        key={key}>[{key+1}]</a>
+                                ))}
+                            </div>
+                        </div>
+                    ) : null
+                }
 
                 {options?.map((data, key) => (
                     <button
                         key={key}
                         onClick={() => {handleChoose(num, key)}}
                         className={`border-2 w-full p-2 m-1 rounded-lg 
-                        ${sel[num]===key ? "border-purple-300 bg-purple-200" : "border-slate-400"}`}>
+                        ${sel[num]===key ? "border-purple-300 bg-purple-200" : "border-slate-200"}`}>
                         {data}
                     </button>
                 ))
@@ -136,7 +158,7 @@ export default function Quiz(){
                         퀴즈
                     </div>
                     <div className="text-center pb-4 font-bold">
-                        {chapterData.type} : &quot;{chapterData.title}&quot;
+                        {chapterData?.type} : &quot;{chapterData?.title}&quot;
                     </div>
                     {
                         quizData?.map((data, key) => (
